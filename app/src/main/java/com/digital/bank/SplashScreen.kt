15547b969ecd8bec881db8f8eb923e50428b273b
Vinicius <viniciuslimaveiga11@gmail.com>
2021-05-05 19:15:47 -0300

    refactor: remove useless code

--- app/src/main/java/com/digital/bank/SplashScreen.kt
+++ app/src/main/java/com/digital/bank/SplashScreen.kt
@@ -1,12 +1,14 @@
 package com.digital.bank
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
 
-//@Suppress("DEPRECATION")
+const val PREFERENCES = "com.digital.bank.PREFERENCE_FILE_KEY"
+
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +20,24 @@ class SplashScreen : AppCompatActivity() {
         )
 
         Handler().postDelayed({
-            val intent = Intent(this, CheckPreferences::class.java)
-            startActivity(intent)
-            finish()
+            isIntroSliderFinished()
         }, 1000)
+    }
+
+    private fun isIntroSliderFinished() {
+        val sharedPref = this.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE)
+        val defaultValue = false
+        val isButtonDonePressed = sharedPref.getBoolean(
+            getString(R.string.preferencekey_onDonePressed_intro),
+            defaultValue
+        )
+
+        if (isButtonDonePressed) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this, Introduction::class.java))
+            finish()
+        }
     }
 }
